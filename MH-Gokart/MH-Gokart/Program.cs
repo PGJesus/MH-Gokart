c8b483d85c70f72e40b828136952fd5c29142c79
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2026-09-07 09:08:25 +0200

    init

---
+++ MH-Gokart/MH-Gokart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH_Gokart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MH-Gokart");
+            Console.WriteLine("2026.09.07.");
+            /*MH-Gokart
+             2026.09.07.
+             */
+
+
+
+        }
+    }
+}
