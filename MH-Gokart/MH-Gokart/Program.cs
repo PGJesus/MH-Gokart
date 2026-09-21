e0936a5addf2481ce5616ad054631761efe8dde9
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2026-09-21 11:26:34 +0200

    szar ez a formazas

--- MH-Gokart/MH-Gokart/Program.cs
+++ MH-Gokart/MH-Gokart/Program.cs
@@ -183,7 +183,13 @@ namespace MH_Gokart
                 naptarak.Add(new Naptar(nap));
             }
 
-            Console.WriteLine("\t\t\t8-9 9-10 10-11 11-12 12-13 13-14 14-15 15-16 16-17 17-18 18-19");
+            Console.Write("\t\t\t");
+            for (int i = 0; i < 11; i++)
+            {
+                string cimke = $"{i + 8,2}-{i + 9}";
+                Console.Write($"{cimke, -6}");
+            }
+            Console.WriteLine();
             foreach (Naptar n in naptarak)
             {
                 n.Kiir();
